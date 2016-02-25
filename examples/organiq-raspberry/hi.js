561a0dfb7432fc755b7c25e7f4135247a2d72797
aandrese <aandrese85@gmail.com>
2016-02-25 13:28:50 -0500

    added a description to hi.js

--- examples/organiq-raspberry/hi.js
+++ examples/organiq-raspberry/hi.js
@@ -1,3 +1,6 @@
+//This is a test for the hardware setup in the organiq-raspberry example
+//It polls the button as fast as it can, and writes the result to the LED
+
 var Gpio = require('onoff').Gpio,
   led = new Gpio(14, 'out'),
   button = new Gpio(4, 'in', 'both');
